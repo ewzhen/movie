b5d74158177b15d1f93f6e97434ab4a1c2179b14
Richard <adf@adf.com>
2012-10-20 14:35:17 +0800

    init commit...

---
+++ app.js
@@ -0,0 +1,57 @@
+var express = require('express'),
+    path = require('path'),
+    http = require('http'),
+    port = process.env.VMC_APP_PORT || 3000,
+    host = process.env.VCAP_APP_HOST || 'localhost',
+    io = require('socket.io'),
+    wine = require('./routes/wines');
+
+var app = express();
+
+app.configure(function () {
+    app.set('port', port);
+    app.set('host', host);
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser())
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+var server = http.createServer(app);
+io = io.listen(server);
+
+
+io.configure(function () {
+    io.set('authorization', function (handshakeData, callback) {
+        if (handshakeData.xdomain) {
+            callback('Cross-domain connections are not allowed');
+        } else {
+            callback(null, true);
+        }
+    });
+});
+
+server.listen(app.get('port'), app.get('host'), function () {
+    console.log("Express server listening on port " + app.get('port'));
+});
+
+app.get('/wines', wine.findAll);
+app.get('/wines/:id', wine.findById);
+app.post('/wines', wine.addWine);
+app.put('/wines/:id', wine.updateWine);
+app.delete('/wines/:id', wine.deleteWine);
+
+io.sockets.on('connection', function (socket) {
+
+    socket.on('message', function (message) {
+        console.log("Got message: " + message);
+        ip = socket.handshake.address.address;
+        url = message;
+        io.sockets.emit('pageview', { 'connections': Object.keys(io.connected).length, 'ip': '***.***.***.' + ip.substring(ip.lastIndexOf('.') + 1), 'url': url, 'xdomain': socket.handshake.xdomain, 'timestamp': new Date()});
+    });
+
+    socket.on('disconnect', function () {
+        console.log("Socket disconnected");
+        io.sockets.emit('pageview', { 'connections': Object.keys(io.connected).length});
+    });
+
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Nodejs movie application #
+
+Start to an application herer
+
+## To run the application on your own Heroku account:##
+
+1. Index
+
+2. View
+
+3. Wiki
+
+        $ kickass
